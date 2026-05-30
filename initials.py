51becd3dc2c61cf00deda2dbe33d26396df5c4a5
katumi <2425401702@live.gctu.edu.gh>
2026-05-30 15:54:53 +0000

    Assignment 2

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
 
-def main():
-  pass #code goes here
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name: Katumi Amadu
+# STUDENT ID:2425401702
+# Email:2425401702@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Katumi Amadu Amadu, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+print()
+
+print("...KAA")
+
+print()
+
+print("...KKKK....KKKK..........AAAAAAAA............AAAAAAAA.......")
+print("...KKKK...KKKK..........AAAAAAAAAA..........AAAAAAAAAA......")
+print("...KKKK..KKKK..........AAA......AAA........AAA......AAA.....")
+print("...KKKK.KKK............AAA......AAA........AAA......AAA.....")
+print("...KKKK.KK.............AAA......AAA........AAA......AAA.....")
+print("...KKKK.KKKK...........AAAAAAAAAAAA........AAAAAAAAAAAA.....")
+print("...KKKK..KKKK..........AAAAAAAAAAAA........AAAAAAAAAAAA.....")
+print("...KKKK...KKKK.........AAA......AAA........AAA......AAA.....")
+print("...KKKK....KKKK...**...AAA......AAA...**...AAA......AAA...**")
+print("...KKKK....KKKK...**...AAA......AAA...**...AAA......AAA...**")
 
 
-
-main()
+print()
